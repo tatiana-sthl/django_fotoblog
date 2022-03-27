55df8c8a82be9c053d94d71ebaac29477e618b80
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-03-27 19:27:21 +0200

    first migration with the User models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Créateur'),
+        (SUBSCRIBER, 'Abonné'),
+    )
+
+    profile_photo = models.ImageField()
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
