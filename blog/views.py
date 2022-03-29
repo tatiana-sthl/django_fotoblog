b520c74c1117c1aecf51d4d766ad1d5744700040
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-03-29 15:33:09 +0200

    add view about blog

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from . import forms, models
+
+@login_required
+def view_blog(request, blog_id):
+    blog = get_object_or_404(models.Blog, id=blog_id)
+    return render(request, "blog/view_blog.html", {"blog": blog})
 
 @login_required
 def photo_upload(request):
@@ -16,6 +21,30 @@ def photo_upload(request):
     return render(request, "blog/photo_upload.html", context={"form": form})
 
 @login_required
+def blog_and_photo_upload(request):
+    blog_form = forms.BlogForm()
+    photo_form = forms.PhotoForm()
+    if request.method == "POST":
+        blog_form = forms.BlogForm(request.POST)
+        photo_form = forms.PhotoForm(request.POST, request.FILES)
+        if all([blog_form.is_valid(), photo_form.is_valid()]):
+            photo = photo_form.save(commit=False)
+            photo.uploader = request.user
+            photo.save()
+            blog = blog_form.save(commit=False)
+            blog.author = request.user
+            blog.photo = photo
+            blog.save()
+            return redirect('home')       
+    context = {
+        "blog_form": blog_form,
+        "photo_form": photo_form,
+    }
+    return render(request, "blog/create_blog_post.html", context=context)
+
+
+@login_required
 def home(request):
     photos = models.Photo.objects.all()
-    return render(request, "blog/home.html", context={"photos": photos})
+    blogs = models.Blog.objects.all()
+    return render(request, "blog/home.html", context={"photos": photos, "blogs": blogs})
