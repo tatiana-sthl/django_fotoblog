d479f6713ab7c1f1256327612c547dbb36e50af9
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-03-30 12:09:05 +0200

    update flux presentation

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,27 @@
+from time import timezone
+from django import template
+
+register = template.Library()
+
+MINUTE = 60
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
+
+@register.filter
+def model_type(value):
+    return type(value).__name__
+
+@register.simple_tag(takes_context=True)
+def get_poster_display(context, user):
+    if user == context['user']:
+        return 'vous'
+    return user.username
+
+@register.filter
+def get_posted_at_display(posted_at):
+    seconds_ago = (timezone.now() - posted_at).total_seconds()
+    if seconds_ago <= HOUR:
+        return f"Publié il y a {int(seconds_ago // MINUTE)} minutes."
+    elif seconds_ago <= DAY:
+        return f"Publié il y a {int(seconds_ago // HOUR)} heures."
+    return f"Publié il y a {int(seconds_ago // DAY)} jours."
